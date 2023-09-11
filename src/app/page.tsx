f0f7d594377a95424efe288185771e91e4123c45
Felzer <warhammer431@mail.ru>
2023-09-11 22:54:25 +0300

    add about block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import { Header } from './components/landing/Header/ui'
+import { About } from './components/landing/About/ui'
  
 
 export default function Home() {
   return (
       <main>
         <Header />
+        <About />
       </main>
   )
 }
