1913bf1cf9c5655d2276d755743e67948531efc9
Felzer <warhammer431@mail.ru>
2023-09-13 23:30:54 +0300

    add lesson (landing)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import { Header } from './components/landing/Header/ui'
 import { About } from './components/landing/About/ui'
+import { Lesson } from './components/Lesson/ui'
+import { Footer } from './components/Footer/ui'
  
 
 export default function Home() {
@@ -9,6 +11,8 @@ export default function Home() {
       <main>
         <Header />
         <About />
+        <Lesson />  
+        <Footer />
       </main>
   )
 }
