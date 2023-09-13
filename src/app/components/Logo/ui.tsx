d800182e0f54fa51b9a19472f00b0175a400d88c
Albert <a.teregulov@bet2u.com>
2023-09-13 15:07:48 +0300

    add footer

--- src/app/components/Logo/ui.tsx
+++ src/app/components/Logo/ui.tsx
@@ -1,16 +1,25 @@
-import { LeftHeaderVector, RightHeaderVector } from "../landing/Vector/ui"
-import styles from './style.module.css'
-import { Oswald } from 'next/font/google'
- 
-export const oswald = Oswald({
-  weight: '700',
-  subsets: ['latin'],
-})
+import { LeftHeaderVector, RightHeaderVector } from "../landing/Vector/ui";
+import styles from "./style.module.css";
+import { Oswald } from "next/font/google";
 
-export const Logo = () => {
-    return (
-        <div className={oswald.className}>
-            <span className={styles.logo}>Тетриум</span>
-        </div>
-    )
+export const oswald = Oswald({
+  weight: "700",
+  subsets: ["latin"],
+});
+
+interface LogoProps {
+  color?: "dark" | "light";
 }
+
+export const Logo = ({ color }: LogoProps) => {
+  return (
+    <div className={oswald.className}>
+      <span
+        className={styles.logo}
+        style={color === "light" ? { color: "#fff" } : { color: "#333" }}
+      >
+        Тетриум
+      </span>
+    </div>
+  );
+};
